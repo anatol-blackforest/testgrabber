bb9e40ca66dc3aee6ad8b67db36d6f1f5e12dbc7
Anatol <anatolblackforest@gmail.com>
2021-01-17 15:58:51 +0200

    order

--- News.sql
+++ News.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS "news" 
 (
-    "__id"   SERIAL , 
+    "id"   SERIAL , 
     "link" VARCHAR(255), 
     "title" VARCHAR(255), 
     "img" VARCHAR(255), 
     "text" TEXT, 
-    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
-    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
-    PRIMARY KEY ("__id")
+    PRIMARY KEY ("id")
 );
